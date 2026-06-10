951af58d2977eee21ca29ba3cb7fc4a7b2ce83d4
Ndikum Sabastine Ndifor <sebastienndifor@gmail.com>
2026-06-10 14:56:47 +0100

    Fix logout functionaloty

--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import NavLinks from './nav-links';
-import { signOut } from 'next-auth/react';
+import { signOut } from '../../auth';
 import { PowerIcon } from '@heroicons/react/24/outline';
 
 export default function SideNav() {
